01999b1aba963ae94af9346a63b046e4b39ba15d
S0ngRu1 <1922909737@qq.com>
2024-09-19 21:58:52 +0800

    feat: update  移除元素

--- 移除元素.py
+++ 移除元素.py
@@ -10,13 +10,17 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
-        temp = []
-        for i in range(len(nums)):
-            if nums[i] != val:
-                temp.append(nums[i])
-        print(f'{len(temp)}, nums = {temp}')
-
+        # temp = []
+        # for i in range(len(nums)):
+        #     if nums[i] != val:
+        #         temp.append(nums[i])
+        # print(f'{len(temp)}, nums = {temp}')
+        for i in range(len(nums)-1,-1,-1):
+            if nums[i] == val:
+                nums.pop(i)
+        print(nums)
+        return len(nums)
 if __name__ == '__main__':
     nums = [3,2,2,3]
     val = 3
-    Solution().removeElement(nums,val)
+    print(Solution().removeElement(nums,val))
